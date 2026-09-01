8ae470510a08ff434ebe48621ada5341ae677712
Jose Castillo <jose.ch@villapet.local>
2026-09-01 16:08:07 -0400

    add: endpoint users

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,6 +5,7 @@ import { DevicesRoutes } from "./devices/devices.routes.js";
 import { GeofencesRoutes } from "./geofences/geofences.routes.js";
 import { PositionsRoutes } from "./positions/positions.routes.js";
 import { VisitsRoutes } from "./visits/visits.routes.js";
+import { UsersRoutes } from "./users/users.routes.js";
 import { extractTraccarCookie } from "../middlewares/extract-traccar-cookie.js";
 
 export class AppRoutes {
@@ -18,6 +19,7 @@ export class AppRoutes {
         router.use('/geofences', extractTraccarCookie, GeofencesRoutes.routes)
         router.use('/positions', extractTraccarCookie, PositionsRoutes.routes)
         router.use('/visits', extractTraccarCookie, VisitsRoutes.routes)
+        router.use('/users', extractTraccarCookie, UsersRoutes.routes)
 
 
         return router;
--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,4 +1,27 @@
 //Session
+export interface UserTraccar {
+    name: string;
+    email: string;
+    password?: string;
+    phone?: string | null;
+    readonly?: boolean;
+    administrator?: boolean;
+    map?: string | null;
+    latitude?: number;
+    longitude?: number;
+    zoom?: number;
+    coordinateFormat?: string | null;
+    disabled?: boolean;
+    expirationTime?: string | null;
+    deviceLimit?: number;
+    userLimit?: number;
+    deviceReadonly?: boolean;
+    limitCommands?: boolean;
+    fixedEmail?: boolean;
+    poiLayer?: string | null;
+    attributes?: Record<string, unknown>;
+}
+
 export interface GeofenceTraccar {
     name: string;
     area: string;
